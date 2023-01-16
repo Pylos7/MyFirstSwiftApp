69ad56f0f234e71cc0bba2294dfb0955a5f93ade
Pylos7 <nestor1ingles@gmail.com>
2023-01-16 15:36:29 -0500

    Initial Commit

---
+++ Shared/SimpleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAppApp.swift
+//  Shared
+//
+//  Created by Nestor Ingles on 1/16/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
